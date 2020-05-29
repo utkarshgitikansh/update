6951de0ddf60fbc4cb0733105b5d8bcfe2ecb575
utkarshgitikansh <utkarshgitikansh@gmail.com>
2020-05-29 11:05:07 +0530

    14th commit

--- app.js
+++ app.js
@@ -66,6 +66,52 @@ app.listen(process.env.PORT || 8085, () => {
     });
 });
 
+app
+  .get("/googlenews", (req, resu) => {
+    https.get(
+      `https://newsapi.org/v2/everything?q=coronavirus%20success%20stories&apiKey=429c0fb561ef41bca3b06bcb3e11fc88`,
+      (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers["content-type"];
+
+        let error;
+        if (statusCode !== 200) {
+          error = new Error("Request Failed.\n" + `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error(
+            "Invalid content-type.\n" +
+              `Expected application/json but received ${contentType}`
+          );
+        }
+        if (error) {
+          console.error(error.message);
+
+          res.resume();
+          return;
+        }
+
+        let rawData = "";
+        res.on("data", (chunk) => {
+          rawData += chunk;
+        });
+        res.on("end", () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            locate = parsedData;
+            resu.send(locate);
+
+            console.log(parsedData);
+          } catch (e) {
+            console.error(e.message);
+          }
+        });
+      }
+    );
+  })
+  .on("error", (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+
 // app.listen(process.env.PORT || 8080, () => {
 //   // at=28.6200469&lng=77.4357733
 
